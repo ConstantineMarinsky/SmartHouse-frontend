56937ccca6d124e2ad7c142600e55a4ebae13900
Dmitry Golysh <disist357@gmail.com>
2016-04-18 18:28:41 +0300

    SH-27 chore (angular bootstrap) remove unused

--- src/index.js
+++ src/index.js
@@ -2,14 +2,6 @@
 import 'core-js';
 import 'zone.js/dist/zone';
 
-// Angular 2
-import 'angular2/platform/browser';
-import 'angular2/platform/common_dom';
-import 'angular2/core';
-import 'angular2/common';
-import 'angular2/http';
-import 'angular2/router';
-
 // RxJS
 import 'rxjs';
 
